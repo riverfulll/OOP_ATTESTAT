1cce82867221668fcfe6e8349d04d12265b5a9bd
Natali <natali23322332@gmail.com>
2024-05-01 20:13:04 +0300

    правки файла mainn

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Maun {
+public class Main {
     public static void main(String[] args) {
         IRaschetFactory RaschetFactory = new LogRaschetFactory(new FileLogger());
         WiewCalculator view = new WiewCalculator(RaschetFactory);
