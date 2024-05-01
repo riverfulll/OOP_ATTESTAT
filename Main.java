3d64655a2d8389945dd5a6204732080dae2c6be1
Natali <natali23322332@gmail.com>
2024-05-01 19:43:06 +0300

    create file - Main

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Maun {
+    public static void main(String[] args) {
+        IRaschetFactory RaschetFactory = new LogRaschetFactory(new FileLogger());
+        WiewCalculator view = new WiewCalculator(RaschetFactory);
+        view.run();
+    }
+}
